aa53cad0ccf5d880f9f0dffce9046f4b96762a15
GabrielSMarcal <gabriel.santos.marcal1@gmail.com>
2025-11-20 10:02:41 -0300

    add CRUD operations for livro route

--- rotas/livro.js
+++ rotas/livro.js
@@ -5,4 +5,16 @@ router.get('/', (req, res) => {
     res.send('Hello World from Moon!')
 })
 
+router.patch('/', (req, res) => {
+    res.send('Teste de PATCH na rota de livros')
+})
+
+router.post('/', (req, res) => {
+    res.send('Teste de POST na rota de livros')
+})
+
+router.delete('/', (req, res) => {
+    res.send('Teste de DELETE na rota de livros')
+})
+
 module.exports = router
