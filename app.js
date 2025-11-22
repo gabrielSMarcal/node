5fd09baa90a419cabbd6cb09e6bfe760393116ad
GabrielSMarcal <gabriel.santos.marcal1@gmail.com>
2025-11-22 11:29:20 -0300

    feat: add CORS support to the application

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const cors = require('cors')
 const express = require('express')
 const rotaLivro = require('./routers/livroRouter')
+
 const app = express()
 const port = 8000
 
 app.use(express.json())
+app.use(cors({origin: '*'}))
 app.use('/livros', rotaLivro)
 
 app.listen(port, () => {
